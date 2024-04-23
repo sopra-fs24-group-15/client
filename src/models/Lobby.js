2f7d8bd129f47df83aa6518a5d56df01bbcfebfd
Marc Timothy Huber <marctimothy.huber@uzh.ch>
2024-04-23 14:28:47 +0200

    #47 corrected indentation issues

--- src/models/Lobby.js
+++ src/models/Lobby.js
@@ -1,12 +1,12 @@
 class Lobby {
-    constructor(data = {}) {
-      this.lobbyid = null;
-      this.players = [];
-      this.lobbyjoincode = null;
-      this.lobbyowner = null;
-      this.gameactive = false;
-      Object.assign(this, data);
-    }
+  constructor(data = {}) {
+    this.lobbyid = null;
+    this.players = [];
+    this.lobbyjoincode = null;
+    this.lobbyowner = null;
+    this.gameactive = false;
+    Object.assign(this, data);
   }
+}
   
-  export default Lobby;
+export default Lobby;
