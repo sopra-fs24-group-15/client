9ed160e9b67b791bca68de744dd9e0675c050550
Marc Timothy Huber <marctimothy.huber@uzh.ch>
2024-05-09 10:15:06 +0200

    #61 added rules for topic mode

--- src/components/ui/Rules.tsx
+++ src/components/ui/Rules.tsx
@@ -7,10 +7,11 @@ export const Rules = ({ close }) => (
     <div className="rules text">
       <h1>RULES</h1>
       <p>MAIN RULE: HAVE FUN</p>
+
       <h3>Basic Mode</h3>
       <ul>
         <li>There is a fixed amount of rounds played, standard: 5</li>
-        <li>You receive a picture, now add your text to make a good meme.</li>
+        <li>Everyones receives a template to create a meme</li>
         <li>Submit your meme and wait for the others.</li>
         <li>Vote for meme you like the most.</li>
         <li>Points</li>
@@ -18,7 +19,25 @@ export const Rules = ({ close }) => (
           <li>Points: 1. 3 Points | 2. 2 Points | 3. 1 Point</li>
           <li>If you receive no vote but are still ranked 2. or 3. you wont receive any points.</li>
         </ul>
+      </ul><br></br>
 
+      <h3>Topic Mode</h3>
+      <ul>
+        <li>There is a fixed amount of rounds played, standard: 5</li>
+        <li>The Player ranked last gets 3 topics an can select one</li>
+        <li>Everyone receives a template and the selected topic to create a meme</li>
+        <li>Submit your meme and wait for the others.</li>
+        <li>Vote for each meme as following:</li>
+        <ul>
+          <li>++ : you like it, it fulfills the topic</li>
+          <li>+- : you like it OR it fulfills the topic (not both)</li>
+          <li>-- : you do not like it and it does not fulfill the topic</li>
+        </ul>
+        <li>Points are given to the top 3 players</li>
+        <ul>
+          <li>Points: 1. 3 Points | 2. 2 Points | 3. 1 Point</li>
+          <li>If you receive no vote but are still ranked 2. or 3. you wont receive any points.</li>
+        </ul>
       </ul>
     </div>
     <button onClick={close} className="rules button">Close</button>
