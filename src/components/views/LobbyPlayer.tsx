c436554fa48cb69fc0f72036541252223e0e773f
Marc Amsler <marc.amsler.02@gmail.com>
2024-05-20 14:47:28 +0200

    #45 chnopf gaddet

--- src/components/views/LobbyPlayer.tsx
+++ src/components/views/LobbyPlayer.tsx
@@ -14,6 +14,8 @@ import rules from "../img/rules.png";
 import home from "../img/home.png";
 // @ts-ignore
 import refresh from "../img/refresh.png";
+// @ts-ignore
+import copy from "../img/copy.png";
 //Rules
 import { Rules } from "../ui/Rules";
 import { LeavePopUp } from "../ui/LeavePopUp";
@@ -128,6 +130,10 @@ const LobbyPlayer = () => {
     return () => clearInterval(intervalId);
   }, []);
 
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(lobbycode);
+  };
+
   return (
     <BaseContainer className="lobby container">
       <div className="home rule">
@@ -149,6 +155,11 @@ const LobbyPlayer = () => {
           <tr className="infoLobbyCode">
             <td>JOIN CODE</td>
             <td className="infoContent">{lobbycode}</td>
+            <td>
+              <button className="copy-button" onClick={copyToClipboard}>
+                <img src={copy} alt="Copy"/>
+              </button>
+            </td>
           </tr>
           <tr>
             <td>GAMEMODE</td>
